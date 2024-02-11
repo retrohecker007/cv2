84fc3e17aba0a8a950193ca33779cf98c55163b9
Naveen Pradeep Kumar Pillai <96583359+retrohecker007@users.noreply.github.com>
2024-02-11 11:33:53 +0530

    Add files via upload

---
+++ colour_filtering_using_trackbars.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+inp = input(r"Enter the image loc: ")
+frame = cv2.imread(inp)
+frame = cv2.resize(frame, (600, 400))
+
+cv2.namedWindow("Trackbars")
+cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
+
+while True:
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+
+    lower_bound = np.array([l_h, l_s, l_v])
+    upper_bound = np.array([u_h, u_s, u_v])
+
+    mask = cv2.inRange(hsv, lower_bound, upper_bound)
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow("Original", frame)
+    cv2.imshow("Mask", mask)
+    cv2.imshow("Result", result)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
